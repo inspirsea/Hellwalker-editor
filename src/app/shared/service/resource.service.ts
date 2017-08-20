525b1dffdbdb8e1079fea7be0dfb898e11943b0d
Emil Sunesson <sunesson.emil@gmail.com>
2017-08-20 12:36:18 +0200

    reemoved old tiles

--- src/app/shared/service/resource.service.ts
+++ src/app/shared/service/resource.service.ts
@@ -17,24 +17,7 @@ export class ResourceService {
     new TextureResource("6tile.png", [32, 32], 6),
     new TextureResource("7tile.png", [32, 32], 7),
     new TextureResource("8tile.png", [32, 32], 8),
-    new TextureResource("9tile.png", [32, 32], 9),
-    new TextureResource("10tile.png", [32, 32], 10),
-    new TextureResource("11tile.png", [32, 32], 11),
-    new TextureResource("12tile.png", [32, 32], 12),
-    new TextureResource("13tile.png", [32, 32], 13),
-    new TextureResource("14tile.png", [32, 32], 14),
-    new TextureResource("15tile.png", [32, 32], 15),
-    new TextureResource("16tile.png", [32, 32], 16),
-    new TextureResource("17tile.png", [32, 32], 17),
-    new TextureResource("18tile.png", [32, 32], 18),
-    new TextureResource("19tile.png", [32, 32], 19),
-    new TextureResource("20tile.png", [32, 32], 20),
-    new TextureResource("21tile.png", [32, 32], 21),
-    new TextureResource("22tile.png", [32, 32], 22),
-    new TextureResource("23tile.png", [32, 32], 23),
-    new TextureResource("24tile.png", [32, 32], 24),
-    new TextureResource("25tile.png", [32, 32], 25),
-    new TextureResource("26tile.png", [32, 32], 26)
+    new TextureResource("9tile.png", [32, 32], 9)
   ];
 
   public enemyTextureResources: TextureResource[] = [
